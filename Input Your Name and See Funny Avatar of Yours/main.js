42f124dcf7842ee09ec27a98c717e6aedc77859d
Shihab Munna <memunna2@gmail.com>
2020-09-04 00:57:31 +0600

    Create main.js

---
+++ Input Your Name and See Funny Avatar of Yours/main.js
@@ -0,0 +1,10 @@
+var element = document.getElementById("btn");
+
+function changeImage()
+{
+    var name =  document.getElementById("name").value;
+    var image = document.getElementById("img");
+    
+    image.src = `https://joeschmoe.io/api/v1/${name}`;
+    
+}
